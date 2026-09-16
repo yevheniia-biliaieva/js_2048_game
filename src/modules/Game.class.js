0415f85589799668e1bcdad62999c39fc69752f0
Yevheniia Biliaieva <biliaieva99@gmail.com>
2026-09-16 15:51:52 +0300

    feat: implement 2048 game logic

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -20,25 +20,80 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
+  constructor(initialState = []) {
+    if (initialState.length === 0) {
+      this.field = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ];
+    } else {
+      this.field = structuredClone(initialState);
+    }
+
+    this.startField = structuredClone(this.field);
+
+    this.score = 0;
+    this.status = 'idle';
+
     // eslint-disable-next-line no-console
     console.log(initialState);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    const newField = this.field.map((row) => {
+      return this.processRow(row);
+    });
+
+    this.updateField(newField);
+  }
+
+  moveRight() {
+    const newField = this.field.map((row) => {
+      const reversedRow = [...row].reverse();
+      const processedRow = this.processRow(reversedRow);
+
+      return processedRow.reverse();
+    });
+
+    this.updateField(newField);
+  }
+
+  moveUp() {
+    const transposedField = this.transposeField(this.field);
+    const newTransposedField = transposedField.map((row) => {
+      return this.processRow(row);
+    });
+
+    this.updateField(this.transposeField(newTransposedField));
+  }
+
+  moveDown() {
+    const transposedField = this.transposeField(this.field);
+    const newTransposedField = transposedField.map((row) => {
+      const reversedRow = [...row].reverse();
+      const processedRow = this.processRow(reversedRow);
+
+      return processedRow.reverse();
+    });
+
+    this.updateField(this.transposeField(newTransposedField));
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.field;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +105,146 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    if (this.status !== 'idle') {
+      return;
+    }
+
+    this.status = 'playing';
+    this.score = 0;
+
+    // Add two random tiles to start the game
+    this.addRandomTile();
+    this.addRandomTile();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.field = structuredClone(this.startField);
+    this.score = 0;
+    this.status = 'idle';
+  }
 
   // Add your own methods here
+
+  addRandomTile() {
+    const emptyCells = [];
+
+    this.field.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        if (cell === 0) {
+          emptyCells.push({ row: rowIndex, col: colIndex });
+        }
+      });
+    });
+
+    if (emptyCells.length === 0) {
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    const randomCell = emptyCells[randomIndex];
+
+    const newValue = Math.random() < 0.9 ? 2 : 4;
+
+    this.field[randomCell.row][randomCell.col] = newValue;
+  }
+
+  processRow(row) {
+    let newRow = row.filter((cell) => cell !== 0);
+
+    // [2,0,2,4] => [2,2,4]
+    for (let i = 0; i < newRow.length - 1; i++) {
+      if (newRow[i] === newRow[i + 1]) {
+        newRow[i] *= 2;
+        this.score += newRow[i];
+        newRow[i + 1] = 0;
+      }
+    }
+
+    newRow = newRow.filter((cell) => cell !== 0);
+
+    while (newRow.length < 4) {
+      newRow.push(0);
+    }
+
+    return newRow;
+  }
+
+  transposeField(field) {
+    const result = [];
+
+    for (let col = 0; col < 4; col++) {
+      const column = [];
+
+      field.forEach((row) => {
+        column.push(row[col]);
+      });
+
+      result.push(column);
+    }
+
+    return result;
+  }
+
+  checkCellsUpdated(updatedField, previousField = this.field) {
+    // Check if any cells have been updated
+    return updatedField.some((row, rowIndex) => {
+      return row.some((cell, colIndex) => {
+        return cell !== previousField[rowIndex][colIndex];
+      });
+    });
+  }
+
+  updateField(newField) {
+    if (this.checkCellsUpdated(newField)) {
+      if (this.status !== 'playing') {
+        return;
+      }
+
+      this.field = newField;
+
+      this.checkWin();
+
+      if (this.status !== 'win') {
+        this.addRandomTile();
+        this.checkLose();
+      }
+    }
+  }
+
+  checkWin() {
+    if (this.field.some((row) => row.some((cell) => cell === 2048))) {
+      this.status = 'win';
+    }
+  }
+
+  checkLose() {
+    const result = this.field.some((row, rowIndex) => {
+      return row.some((cell, colIndex) => {
+        const canCheckRight = colIndex < row.length - 1;
+        const canCheckDown = rowIndex < this.field.length - 1;
+
+        return (
+          (canCheckRight && cell === row[colIndex + 1]) ||
+          (canCheckDown && cell === this.field[rowIndex + 1][colIndex])
+        );
+      });
+    });
+
+    if (!result && this.field.every((row) => row.every((cell) => cell !== 0))) {
+      this.status = 'lose';
+    }
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,92 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
 // Write your code here
+const startButton = document.querySelector('.start');
+const allDomCells = document.querySelectorAll('.field-cell');
+const scoreElement = document.querySelector('.game-score');
+const startMessage = document.querySelector('.message-start');
+const winMessage = document.querySelector('.message-win');
+const loseMessage = document.querySelector('.message-lose');
+
+function updateMessage() {
+  const gameStatus = game.getStatus();
+
+  switch (gameStatus) {
+    case 'idle':
+      startMessage.classList.remove('hidden');
+      winMessage.classList.add('hidden');
+      loseMessage.classList.add('hidden');
+      break;
+    case 'playing':
+      startMessage.classList.add('hidden');
+      break;
+    case 'win':
+      winMessage.classList.remove('hidden');
+      break;
+    case 'lose':
+      loseMessage.classList.remove('hidden');
+      break;
+  }
+}
+
+function updateScore(score) {
+  scoreElement.textContent = score;
+}
+
+function renderField(elements) {
+  const cellsValue = game.getState().flat();
+
+  cellsValue.forEach((cell, index) => {
+    elements[index].textContent = cell === 0 ? '' : cell;
+  });
+}
+
+startButton.addEventListener('click', () => {
+  if (startButton.classList.contains('restart')) {
+    game.restart();
+    startButton.classList.remove('restart');
+    startButton.textContent = 'Start';
+  } else {
+    game.start();
+  }
+
+  renderField(allDomCells);
+  updateScore(game.getScore());
+  updateMessage();
+
+  if (game.getStatus() === 'playing') {
+    startButton.classList.add('restart');
+    startButton.textContent = 'Restart';
+  }
+});
+
+document.addEventListener('keydown', (ev) => {
+  if (game.getStatus() !== 'playing') {
+    return;
+  }
+
+  const key = ev.key;
+
+  switch (key) {
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+  }
+
+  renderField(allDomCells);
+  updateScore(game.getScore());
+  updateMessage();
+});
